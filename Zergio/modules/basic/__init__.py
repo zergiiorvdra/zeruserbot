25f52598dfe993455b7b18d9a00815403b73bd93
zergiogithub <138329164+zergiorvdr@users.noreply.github.com>
2023-08-02 10:50:34 +0700

    Update __init__.py

--- Zergio/modules/basic/__init__.py
+++ Zergio/modules/basic/__init__.py
@@ -5,13 +5,14 @@ from pyrogram import Client
 
 
 def restart():
-    os.execvp(sys.executable, [sys.executable, "-m", "PyroKar"])
+    os.execvp(sys.executable, [sys.executable, "-m", "Zergio"])
 
 async def join(client):
     try:
         await client.join_chat("mutualan_cari_teman_virtual")
         await client.join_chat("RuangGabutArman")
         await client.join_chat("zerrcode")
+        await client.join_chat("StoreKarman")
         await client.join_chat("zersupport_bot_telegram")
         await client.join_chat("StoryMan01")
     except BaseException:
