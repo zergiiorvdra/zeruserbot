ce06295c0bb366be0ce6519615616b8653e22c5b
zer-Pyro <143489626+zergiiorvdra@users.noreply.github.com>
2024-02-28 06:20:36 +0800

    Update bot.py

--- Zergio/modules/bot/bot.py
+++ Zergio/modules/bot/bot.py
@@ -61,7 +61,7 @@ async def on_plug_in_cb(_, callback_query: CallbackQuery):
     this_command = f"──「 **Help For {str(modul_name).upper()}** 」──\n\n"
     for x in commands:
         this_command += f"  •  **Command:** `.{str(x)}`\n  •  **Function:** `{str(commands[x])}`\n\n"
-    this_command += "© @ZERGIIORVDRA"
+    this_command += "© @CTVASELOLE"
     bttn = [
         [InlineKeyboardButton(text="Return", callback_data="reopen")],
     ]
