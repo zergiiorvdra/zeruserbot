956c81895207882b1a90f2cb4ebffcf5c4fc0ecf
zer-Pyro <143489626+zergiiorvdra@users.noreply.github.com>
2024-03-02 02:41:49 +0800

    Add files via upload

---
+++ Zergio/modules/animation2.py
@@ -0,0 +1,168 @@
+# Credits: @mrismanaziz
+# Copyright (C) 2022 Pyro-ManUserbot
+#
+# This file is a part of < https://github.com/mrismanaziz/PyroMan-Userbot/ >
+# PLease read the GNU Affero General Public License in
+# <https://www.github.com/mrismanaziz/PyroMan-Userbot/blob/main/LICENSE/>.
+#
+# t.me/SharingUserbot & t.me/Lunatic0de
+
+import asyncio
+import random
+
+import requests
+from pyrogram import *
+from pyrogram import Client, filters
+from pyrogram.errors.exceptions.flood_420 import FloodWait
+from pyrogram.types import *
+from pyrogram.types import Message
+
+from config import CMD_HANDLER as cmd
+from Zergio.helpers.basic import edit_or_reply, get_text
+from Zergio.helpers.constants import MEMES
+
+from .help import *
+
+DEFAULTUSER = "ZerxSep"
+
+@Client.on_message(filters.command(["ange", "tegang"], cmd) & filters.me)
+async def ange(client: Client, message: Message):
+        typew = await edit_or_reply(message, "SAYANGGGGGGGGG 💕")
+        await asyncio.sleep(1)
+        await typew.edit("💝💘💓💗")
+        asyncio.sleep(1)
+        await typew.edit("💞💕💗💘")
+        asyncio.sleep(1)
+        await typew.edit("💝💘💓💗")
+        asyncio.sleep(1)
+        await typew.edit("💞💕💗💘")
+        asyncio.sleep(1)
+        await typew.edit("💘💞💗💕")
+        asyncio.sleep(1)
+        await typew.edit("💘💞💕💗")
+        asyncio.sleep(1)
+        await typew.edit("EMMMMMM🥺🥺🥺")
+        asyncio.sleep(1)
+        await typew.edit("💝💘💓💗")
+        await typew.edit("💞💕💗💘")
+        await typew.edit("💘💞💕💗")
+        await typew.edit("SAYANG")
+        asyncio.sleep(1)
+        await typew.edit("AKU 👉👈")
+        asyncio.sleep(1)
+        await typew.edit("SANGE 👉👈 😘😘")
+        asyncio.sleep(1)
+        await typew.edit("💘💘💘💘")
+        asyncio.sleep(1)
+        await typew.edit("SAYANG")
+        asyncio.sleep(1)
+        await typew.edit("AYO NGEWE🤭🤭")
+        asyncio.sleep(1)
+        await typew.edit("PLISS🥺🥺")
+        asyncio.sleep(1)
+        await typew.edit("AKU SANGE😋😋")
+        asyncio.sleep(1)
+        await typew.edit("I LOVE YOUUUU")
+        asyncio.sleep(1)
+        await typew.edit("AH AH AH BEIBB")
+        asyncio.sleep(1)
+        await typew.edit("💦💦💦💦")
+        asyncio.sleep(1)
+        await typew.edit("OH BABY")
+        asyncio.sleep(1)
+        await typew.edit("AKU SAYANG KAMU💞")
+
+
+@Client.on_message(filters.command(["orgil"], cmd) & filters.me)
+async def orgil(client: Client, message: Message):
+    typew.pattern_match.group(1)
+    await typew.edit("`ANJING ADA ORANG GILA.....`")
+    asyncio.sleep(1)
+    await typew.edit("`ORANG GILAAAAAA!!`")
+    asyncio.sleep(1)
+    await typew.edit("`🏃                        🤸`")
+    await typew.edit("`🏃                       🤸`")
+    await typew.edit("`🏃                      👨‍🦽`")
+    await typew.edit("`🏃                     ⛹️`")
+    await typew.edit("`🏃   `LARII`          🤾`")
+    await typew.edit("`🏃                   🤾`")
+    await typew.edit("`🏃                  🤾`")
+    await typew.edit("`🏃                 🤾`")
+    await typew.edit("`🏃                🤾`")
+    await typew.edit("`🏃               🤺`")
+    await typew.edit("`🏃              🏊`")
+    await typew.edit("`🏃             🏊`")
+    await typew.edit("`🏃            🏄`")
+    await typew.edit("`🏃           🤾`")
+    await typew.edit("`🏃PULUPULU   🧚`")
+    await typew.edit("`🏃           ⛹️`")
+    await typew.edit("`🏃            ⛹️`")
+    await typew.edit("`🏃             🤺`")
+    await typew.edit("`🏃              🥴`")
+    await typew.edit("`🏃               🏃`")
+    await typew.edit("`🏃                🏃`")
+    await typew.edit("`🏃                 🤸`")
+    await typew.edit("`🏃                  🤸`")
+    await typew.edit("`🏃                   🤸`")
+    await typew.edit("`🏃                    🤸`")
+    await typew.edit("`🏃                     ⛹️`")
+    await typew.edit("`🏃  Huh-Huh           🏃`")
+    await typew.edit("`🏃                   🤑`")
+    await typew.edit("`🏃                  🙈`")
+    await typew.edit("`🏃                 ⛹️`")
+    await typew.edit("`🏃                🏃`")
+    await typew.edit("`🏃               🤴`")
+    await typew.edit("`🏃              🐖`")
+    await typew.edit("`🏃             🐖`")
+    await typew.edit("`🏃            🥴`")
+    await typew.edit("`🏃           🥴`")
+    await typew.edit("`🏃          🤡`")
+    await typew.edit("`🏃         🤭`")
+    await typew.edit("`CAPE BANGET ANJING!!!`")
+    asyncio.sleep(1)
+    await typew.edit("`🏃       🏃`")
+    await typew.edit("`🏃      🤾`")
+    await typew.edit("`🏃     🏃`")
+    await typew.edit("`🏃    🏃`")
+    await typew.edit("`Dahlah Pasrah Aja`")
+    asyncio.sleep(1)
+    await typew.edit("`🧎🐖`")
+    asyncio.sleep(2)
+    await typew.edit("`-TAMAT-`")
+
+
+@Client.on_message(filters.command(["sadboy"], cmd) & filters.me)
+async def sadboy(client: Client, message: Message):
+    typew.pattern_match.group(1)
+    asyncio.sleep(2)
+    await typew.edit("`Pertama-tama kamu cantik`")
+    asyncio.sleep(2)
+    await typew.edit("`Kedua kamu manis`")
+    asyncio.sleep(1)
+    await typew.edit("`Dan yang terakhir adalah kamu bukan jodohku`")
+
+@Client.on_message(filters.command(["pantau"], cmd) & filters.me)
+async def pantau(client: Client, message: Message):
+    await edit_or_reply(
+        "`\n┻┳|―-∩`"
+        "`\n┳┻|     ヽ`"
+        "`\n┻┳|    ● |`"
+        "`\n┳┻|▼) _ノ`"
+        "`\n┻┳|￣  )`"
+        "`\n┳ﾐ(￣ ／`"
+        "`\n┻┳T￣|`"
+        "\n**Masih Ku Pantau**"
+    )
+
+
+
+add_command_help(
+    "animation",
+    [
+        ["ange", "Untuk menampilkan animasi ngemis anu."],
+        ["orgil", "Untuk menampilkan animasi orang gila lari larian."],
+        ["sadboy", "Untuk menampilkan animasi sadboy."],
+        ["pantau", "Untuk menampilkan art lagi mantau."],
+        
+    ],
+)
